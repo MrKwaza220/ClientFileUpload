c72be3995b171362fa49713b0d78f1b8c537f2a9
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-24 12:21:55 +0200

    added max file size

--- src/main/java/com/enviro/assessment/grad001/sakhumzikwaza/clientfileupload/FileController.java
+++ src/main/java/com/enviro/assessment/grad001/sakhumzikwaza/clientfileupload/FileController.java
@@ -1,6 +1,9 @@
 package com.enviro.assessment.grad001.sakhumzikwaza.clientfileupload;
 
 
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.repository.query.Param;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.swing.text.html.Option;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class FileController {
@@ -43,5 +48,27 @@ public class FileController {
                 "The file uploaded successfu");
 
         return "redirect:/";
+    }
+
+    @GetMapping("/download")
+    public void downloadfile(@Param("id") Long id, HttpServletResponse response) throws Exception {
+        Optional<Document> result = repo.findById(id);
+        if(!result.isPresent()){
+            throw new Exception("Could not find document with ID: " + id);
+            
+        }
+
+        Document document = result.get();
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=" + document.getName();
+
+        response.setHeader(headerKey, headerValue);
+        ServletOutputStream outputStream = response.getOutputStream();
+
+        outputStream.write(document.getContent());
+        outputStream.close();
+
+
     }
 }
