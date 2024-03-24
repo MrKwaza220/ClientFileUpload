175d7feb34c9005afe2daf5e7893526c3a8e5b72
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-24 03:16:40 +0200

    added message that will diplay after the file is uploaded

--- src/main/java/com/enviro/assessment/grad001/sakhumzikwaza/clientfileupload/FileController.java
+++ src/main/java/com/enviro/assessment/grad001/sakhumzikwaza/clientfileupload/FileController.java
@@ -1,14 +1,42 @@
 package com.enviro.assessment.grad001.sakhumzikwaza.clientfileupload;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.Date;
 
 @Controller
 public class FileController {
+    @Autowired
+    private DocumentRepository repo;
 
     @GetMapping("/")
     public String viewHomePage(){
         return "home";
 
+    }
+    @PostMapping("upload")
+    public String uploadFile(@RequestParam("client_files")MultipartFile multipartFile,
+                             RedirectAttributes ra) throws IOException {
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        Document document = new Document();
+        document.setName(fileName);
+        document.setContent(multipartFile.getBytes());
+        document.setSize(multipartFile.getSize());
+        document.setUploadTime(new Date());
+
+        repo.save(document);
+
+        ra.addFlashAttribute("message",
+                "The file uploaded successfu");
+
+        return "redirect:/";
     }
 }
