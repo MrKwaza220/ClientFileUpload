04cda19bdc5583695b04072467700111acd5b6e7
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-25 19:19:08 +0200

    add changes to repofile

--- src/main/java/com/enviro/assessment/grad001/sakhumzikwaza/clientfileupload/DocumentRepository.java
+++ src/main/java/com/enviro/assessment/grad001/sakhumzikwaza/clientfileupload/DocumentRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+  // Custom query to select specific fields from Document entity
   @Query("SELECT new Document(d.id, d.name, d.size) FROM Document d ORDER BY d.uploadTime DESC")
   List<Document> findAll();
 }
